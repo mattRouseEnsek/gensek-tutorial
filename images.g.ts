d590194fe47b58bc15b835d2229440c67d5d3737
mattRouseEnsek <108789222+mattRouseEnsek@users.noreply.github.com>
2025-08-13 14:50:48 +0100

    Update

---
+++ images.g.ts
@@ -0,0 +1,82 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image3":
+            case "pipetop":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+f f f f f f f f f f f f f f f f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f f f f f f f f f f f f f f f f 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+`;
+            case "image2":
+            case "pipe":return img`
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+. . f 7 7 7 7 7 7 7 7 7 7 f . . 
+`;
+            case "image1":
+            case "battery":return img`
+. . . . . f f f f f f . . . . . 
+. . . f f f f f f f f f f . . . 
+. . . f 4 4 4 4 4 4 4 4 f . . . 
+. . . f 4 4 4 f f 4 4 4 f . . . 
+. . . f 4 4 f f f f 4 4 f . . . 
+. . . f 4 4 f f f f 4 4 f . . . 
+. . . f 4 4 4 f f 4 4 4 f . . . 
+. . . f 4 4 4 4 4 4 4 4 f . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ tilemap.g.ts
@@ -0,0 +1,51 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`400008000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000000000040000000400040004000000040002000400040004000004000004000100000400000400040000040000000400000000000000000000000000000000000004000002000000000000000000000000010000000000000000000000000001000000000000000000000000000000000000000000000000000005000003030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303`, img`
+................................................................
+................................................................
+................................................................
+................................................................
+................................................................
+................................................................
+................................................................
+................................................................
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "pipe_tile":
+            case "tile1":return tile1;
+            case "pipetop_tile":
+            case "tile2":return tile2;
+            case "floor_tile":
+            case "tile3":return tile3;
+            case "battery_tile":
+            case "tile4":return tile4;
+            case "flag_tile":
+            case "tile5":return tile5;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
